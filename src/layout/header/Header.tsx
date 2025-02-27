6f46ab166363a7cd2fc472f9cf4239ca5e942175
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-02-28 00:34:48 +0300

    feat(hw_1): add logo

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/logo/logo (1).svg";
 
 export const Header = () => {
-  return <header>kjbkjb</header>;
+  return (
+    <header>
+      <div>
+        <Logo src={logo} alt="" />
+      </div>
+    </header>
+  );
 };
+
+export const Logo = styled.img`
+  background-color: black;
+`;
